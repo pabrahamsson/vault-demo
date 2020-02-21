e982b481a08cb1a91a09463346e257bb185904eb
Petter Abrahamsson <pabraham@redhat.com>
2020-02-21 15:43:54 -0500

    initial commit

---
+++ demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import hvac
+from flask import Flask
+
+demo = Flask(__name__)
+
+@demo.route('/')
+def secrets():
+    client = hvac.Client(url='https://pa-vault.apps.s11.core.rht-labs.com')
+    f = open('/var/run/secrets/kubernetes.io/serviceaccount/token')
+    jwt = f.read()
+    client.auth_kubernetes(role="app-role", jwt=jwt, mount_point="kubernetes-demo")
+
+    secret_path = 'producta/servicea'
+    kv_secret = client.secrets.kv.read_secret_version(path=secret_path)
+
+if __name__ == '__main__':
+    demo.run()
